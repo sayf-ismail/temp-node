190dadc10b53bb5eaa62d118add7daf0cbf8673a
Sayf Ismail <sayfti@gmail.com>
2021-04-17 17:13:24 +0800

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,25 @@
+const {readFile, writeFile} = require('fs');
+// provide a callback for async - it runs when we are done
+console.log('start');
+readFile('./content/first.txt', 'utf-8',(err, result) => {
+  if(err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile('./content/second.txt','utf-8',(err,result) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile('./content/result-async.txt',`Here is the result : ${first}, ${second}`, (err, result) => {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      console.log('done with this task');
+    })
+  })
+})
+console.log('starting the next task');
